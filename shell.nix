c9e3c146f9ffe621cb075927eb43594fa2282f18
Heliwrenaid <soulh24xda@gmail.com>
2024-12-22 23:35:14 +0100

    ci: fix coverage job

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a9b86fc2290b69375c5542b622088eb6eca2a7c3.tar.gz") {}}:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy ];
+  nativeBuildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy llvmPackages_17.libllvm ];
   buildInputs = with pkgs; [ 
     php
     lua
@@ -10,4 +10,11 @@ pkgs.mkShell {
     jdk17
   ];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  shellHook = ''
+    export LLVM_COV=$(which llvm-cov)
+    export LLVM_PROFDATA=$(which llvm-profdata)
+    if [ -z $(cargo --list | grep llvm-cov) ]; then
+      cargo install cargo-llvm-cov
+    fi
+  '';
 }
